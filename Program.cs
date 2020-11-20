546fbeae19cf516d715d8a6404965cc104c64dfc
Mahery05 <mahery.razakandraina@ynov.com>
2020-11-20 22:09:01 +0100

    envoi du projet

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            Projet ex = new Projet();
+
+            Task t = ex.soustitre("Le Trône de fer");
+            await t;
+            Task t2 = ex.soustitre2("Estimez-vous heureux");
+            await t2;
+            Task t3 = ex.soustitre3("Vos couilles ne geleront pas");
+            await t3;
+            Task t4 = ex.soustitre4("Vous méprisez les blagues de nains, pas celles d'eunuques");
+            await t4;
+            Task t5 = ex.soustitre5("Parce que j'ai des couilles et pas vous");
+            await t5;
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
